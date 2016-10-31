a5ea2f2dac88d28881f8ca3993541683c900d767
mittelmabigkeit <comp422012016@mail.ru>
2016-10-31 17:28:24 +0300

    backbone

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/**
+ * Created by html on 31.10.2016.
+
+var Person = function(config){
+    this.name = config.name;
+    this.age = config.age;
+    this.job = config.job;
+};
+
+Person.prototype.walk = function(){
+  return this.name + ' is walking';
+};
+
+var nick = new Person({name: 'Nick', age:'24',job:'Front-end-developer'});
+ */
+var Person = Backbone.Model.extend({// на жаргоне это класс
+    defaults: {
+        name: 'Dima',
+        age: 23,
+        job: 'web-developer'
+    },
+
+    validate: function(attrs) {
+        console.log(attrs);
+
+        //if (!attrs.name){
+           // return "Имя не должно быть пустым";
+        //}
+
+
+
+        if ( attrs.age <= 0 ) {
+          return 'Возраст должен быть положительным !';
+        }
+    }
+
+   // walk:function(){
+   //     return this.get('name')+ ' is walking'
+   // }
+
+});
+
+//var person = new Person;
